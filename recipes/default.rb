e340f1374bed515548bfbc89d409231a8a52ba9e
Joe Gardiner <joe@grdnr.io>
2016-06-07 11:32:26 +0100

    Rubcop scan and config to pass

--- recipes/default.rb
+++ recipes/default.rb
@@ -7,6 +7,6 @@
 include_recipe 'base-win2012-hardening::access'
 include_recipe 'base-win2012-hardening::ie'
 include_recipe 'base-win2012-hardening::rdp'
-include_recipe 'base-win2012-hardening::audit-logging'
-include_recipe 'base-win2012-hardening::account-lockout'
+include_recipe 'base-win2012-hardening::audit_logging'
+include_recipe 'base-win2012-hardening::account_lockout'
 include_recipe 'base-win2012-hardening::password'
