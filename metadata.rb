792b4f34f70394022d4f4ad7b1c2f885a52e862c
Joe Gardiner <joe@grdnr.io>
2016-06-08 10:51:46 +0100

    Refactor recipe names for Rubocop

--- metadata.rb
+++ metadata.rb
@@ -3,6 +3,5 @@ maintainer 'Joe Gardiner'
 maintainer_email 'joe@chef.io'
 license 'all_rights'
 description 'Hardneing cookbook for Windows 2012 R2'
-long_description 'Remediates critical issues identified by the Windows base
-profile in Chef Compliance.'
-version '0.6.3'
+long_description 'Remediates critical issues identified by the Windows base profile in Chef Compliance.'
+version '0.7.1'
