86db97ebf6321fc57bc36912f7a96062cace4253
Joe Gardiner <joe@grdnr.io>
2016-11-11 15:35:47 +0000

    Added powershell get module package installation

---
+++ recipes/00_base_config.rb
@@ -0,0 +1,22 @@
+#
+# Cookbook Name:: base-win2012-hardening
+# Recipe:: 00_base_config
+#
+# Copyright (c) 2016 Joe Gardiner, All Rights Reserved.
+
+case node['kernel']['os_info']['os_architecture']
+when '64-bit'
+  windows_package 'powerShellGet module 64' do
+    source 'https://download.microsoft.com/download/C/4/1/C41378D4-7F41-4BBE-9D0D-0E4F98585C61/PackageManagement_x64.msi'
+    installer_type :msi
+    action :install
+    options '/quiet /norestart'
+  end
+when '32-bit'
+  windows_package 'powerShellGet module 32' do
+    source 'https://download.microsoft.com/download/C/4/1/C41378D4-7F41-4BBE-9D0D-0E4F98585C61/PackageManagement_x86.msi'
+    installer_type :msi
+    action :install
+    options '/quiet /norestart'
+  end
+end
