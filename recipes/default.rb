37f2312589c9cabcee5c7a8ea96684681d4b83ee
Joe Gardiner <joe@grdnr.io>
2016-12-02 10:00:27 +0000

    removed based config

--- recipes/default.rb
+++ recipes/default.rb
@@ -4,11 +4,10 @@
 #
 # Copyright (c) 2016 Joe Gardiner, All Rights Reserved.
 
-include_recipe 'base-win2012-hardening::00_base_config'
-#include_recipe 'base-win2012-hardening::01_password_policy'
-#include_recipe 'base-win2012-hardening::02_account_lockout'
-#include_recipe 'base-win2012-hardening::03_user_rights'
-#include_recipe 'base-win2012-hardening::04_audit'
-#include_recipe 'base-win2012-hardening::05_ie'
-#include_recipe 'base-win2012-hardening::07_rdp'
-#include_recipe 'base-win2012-hardening::08_access'
+include_recipe 'base-win2012-hardening::01_password_policy'
+include_recipe 'base-win2012-hardening::02_account_lockout'
+include_recipe 'base-win2012-hardening::03_user_rights'
+include_recipe 'base-win2012-hardening::04_audit'
+include_recipe 'base-win2012-hardening::05_ie'
+include_recipe 'base-win2012-hardening::07_rdp'
+include_recipe 'base-win2012-hardening::08_access'
