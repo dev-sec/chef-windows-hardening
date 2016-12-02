86dc9c3e83f3656b3cfa2da49447686132bfa930
Joe Gardiner <joe@grdnr.io>
2016-12-02 10:00:18 +0000

    completed secedit template import

--- recipes/02_account_lockout.rb
+++ recipes/02_account_lockout.rb
@@ -7,23 +7,18 @@
 # start of fix for cis-account-lockout-duration-1.2.1,
 # cis-reset-account-lockout-1.2.3, windows-account-104 windows-account-100,
 # cis-add-workstations-2.2.4
-template 'C:\newSecurityPolicy.txt' do
-  source 'newSecurityPolicy.erb'
+template 'C:\windows\security\templates\hardeningSecurityPolicy.inf' do
+  source 'hardeningSecurityPolicy.erb'
   rights :read, 'Everyone'
-#  notifies :run, 'execute[Import security policy settings]', :immediately
+  action :create
 end
 
-powershell_script 'Import security policy settings' do
-#  command 'secedit.exe /configure /db "secedit.sdb" /cfg C:\newSecurityPolicy.txt'
-  code 'secedit /configure /db secedit.sdb /cfg C:/newSecurityPolicy.txt /overwrite /quiet'
-#  action :nothing
-end
-
-execute 'Refresh policy settings' do
-  command 'secedit /refreshpolicy machine_policy /enforce /quiet'
+execute 'Import security policy settings' do
+  command 'Secedit /configure /db hardening.sdb /cfg C:\windows\security\templates\hardeningSecurityPolicy.inf'
+  cwd 'C:/Windows/security/database'
+  live_stream true
   action :run
 end
-
 # end of fix for cis-account-lockout-duration-1.2.1,
 # cis-reset-account-lockout-1.2.3, windows-account-104 windows-account-100,
 # cis-add-workstations-2.2.4
