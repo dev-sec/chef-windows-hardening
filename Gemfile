58ddcd84bc8717a1680f44997914600575e70d53
Christoph Hartmann <chris@lollyrock.com>
2017-03-11 00:15:51 +0100

    add appveyor testing

---
+++ Gemfile
@@ -0,0 +1,18 @@
+source 'https://rubygems.org'
+
+group :test do
+  gem 'rubocop', '~> 0.44.0'
+  gem 'highline', '~> 1.6.0'
+end
+
+group :integration do
+  gem 'berkshelf'
+  gem 'kitchen-inspec'
+  gem 'test-kitchen'
+  gem 'kitchen-vagrant'
+  gem 'inspec', '~> 1'
+end
+
+group :tools do
+  gem 'github_changelog_generator', '~> 1.12.0'
+end
--- recipes/default.rb
+++ recipes/default.rb
@@ -4,10 +4,10 @@
 #
 # Copyright (c) 2016 Joe Gardiner, All Rights Reserved.
 
-include_recipe 'base-win2012-hardening::01_password_policy'
-include_recipe 'base-win2012-hardening::02_account_lockout'
-include_recipe 'base-win2012-hardening::03_user_rights'
-include_recipe 'base-win2012-hardening::04_audit'
-include_recipe 'base-win2012-hardening::05_ie'
-include_recipe 'base-win2012-hardening::07_rdp'
-include_recipe 'base-win2012-hardening::08_access'
+include_recipe 'windows-hardening::01_password_policy'
+include_recipe 'windows-hardening::02_account_lockout'
+include_recipe 'windows-hardening::03_user_rights'
+include_recipe 'windows-hardening::04_audit'
+include_recipe 'windows-hardening::05_ie'
+include_recipe 'windows-hardening::07_rdp'
+include_recipe 'windows-hardening::08_access'
